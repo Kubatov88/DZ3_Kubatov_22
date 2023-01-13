25b1e94e9e0ef81456b6a70b505a4f9b48f179cb
Sultan <119931354+Kubatov88@users.noreply.github.com>
2023-01-13 10:50:11 +0600

    Add files via upload

---
+++ DZ3_Kubatov_22/app.js
@@ -0,0 +1,40 @@
+// var number1 = Number(prompt("Ведите Первое число"))
+// var number2 = Number(prompt("Ведите Второе число"))
+// function lol() {
+//     if(isNaN(number1) || isNaN(number2)) console.log('Ввели число!');
+//     else {
+//         if(number1<number2) console.log("Число " + number1 + " Меньше")
+//         else if(number1===number2) console.log("Равны")
+//         else console.log("Число " + number2 + " меньше")
+//     }
+// }
+// lol()
+
+
+
+// let str = prompt("Ввести первое число")
+// const strLern = (s) => {
+//     return s.length
+    
+// }
+// console.log(strLern(str));
+
+
+
+// let one = Number(prompt('Ввести 1 число'))
+// let two = Number(prompt('Ввести 2 число'))
+
+// const calculator = (a, b) => {
+//     console.log(a + b + ' сложение')
+//     console.log(a - b + ' вычитание')
+//     console.log(a * b + ' умножение')
+//     console.log(a / b + ' деление')
+
+// }
+
+// calculator(one, two)
+
+
+
+
+
